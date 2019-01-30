9413c28b7db858dbd6493dc3eb22cd61082b7e52
Frédéric Trouche <fred@linuxtribe.org>
2019-01-30 08:09:17 +0100

    Updated installation prefix
    INSTALL file updated (autotools). Default prefix path is now /tmp/inst in appveyor-build.sh

--- share/appveyor-build.sh
+++ share/appveyor-build.sh
@@ -1,29 +1,37 @@
 #!/bin/sh
 
-source /appveyor.environment
-export MSYSTEM
+app_env=/appveyor.environment
+if test -f $app_env; then
+	source $app_env
+	export MSYSTEM
+fi
 
-cd /c/projects/src
+cd ..
 ./autogen.sh
-
-./configure --prefix /inst/
+./configure --prefix /tmp/inst/
 
 v=$(grep goffice_full_version config.h | cut -f2 -d '"')
-v="$v-$(date +'%Y.%m.%d-%H')"
-powershell.exe -command "Update-AppveyorBuild -Version \"$v\""
+
+if test -f $app_env; then
+	v="$v-$(date +'%Y.%m.%d-%H')"
+	powershell.exe -command "Update-AppveyorBuild -Version \"$v\""
+fi
+
 echo "Version=$v"
 
 make -j 2
 make install
 
-if [ $MSYSTEM = "MINGW32" ]; then
-	bits="32bit"
-else
-	bits="64bit"
+if test -f $app_env; then
+	if [ $MSYSTEM = "MINGW32" ]; then
+		bits="32bit"
+	else
+		bits="64bit"
+	fi
+
+	fn="goffice-$bits-$v-archive.zip"
+
+	zip -9r  $fn /tmp/inst/*
+
+	powershell.exe -command "Push-AppveyorArtifact \"$fn\" -DeploymentName \"goffice\""
 fi
-
-fn="goffice-$bits-$v-archive.zip"
-
-zip -9r  $fn /inst/*
-
-powershell.exe -command "Push-AppveyorArtifact \"$fn\" -DeploymentName \"goffice\""
